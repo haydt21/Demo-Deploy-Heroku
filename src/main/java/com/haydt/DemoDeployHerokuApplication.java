f690f0529351ab0cb4d2779691748920814a1810
haydt2104 <144002142+haydt2104@users.noreply.github.com>
2023-09-06 16:50:45 +0700

    init

---
+++ src/main/java/com/haydt/DemoDeployHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.haydt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployHerokuApplication.class, args);
+	}
+
+}
