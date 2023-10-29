b0bf44ccda43b86aca54e9181a1d965eaf2d2594
haydt2104 <144002142+haydt2104@users.noreply.github.com>
2023-10-29 13:59:37 +0700

    test update

--- src/main/java/com/haydt/controller/HomeController.java
+++ src/main/java/com/haydt/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String index() {
-		return"hello haydt";
+		return"hello haydt 21/04/2003";
 	}
 	
 	@GetMapping("/home")
